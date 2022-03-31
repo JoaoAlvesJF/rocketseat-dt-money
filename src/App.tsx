befe3a60328957fb9db50a3d82e1a97d3075325f
JoaoAlvesJF <JoaoAlves.JFAL@outlook.com>
2022-03-31 01:37:46 -0300

    criando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from "./styles/global";
-
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-      <h1>Olar!</h1>
-    </div>
+    </>
   );
 }
 
